22d928d555764094e6f4aa5f133c0d02e617234e
emeraldkid835 <gamedestroyer835@gmail.com>
2024-01-30 09:49:01 -0600

    Initial commit

---
+++ Assets/StarterAssets/Scripts/MovingPlatform.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField]
+    private WaypointPath _waypointPath;
+    [SerializeField]
+    private float _speed;
+
+    private int _targetWaypointIndex;
+
+    private Transform _previousWaypoint;
+    private Transform _targetWaypoint;
+
+    private float _timeToWaypoint;
+    private float _elapsedTime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

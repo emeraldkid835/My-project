43821248344cfd05ef7e33f40b1a376e1551d1dd
emeraldkid835 <gamedestroyer835@gmail.com>
2024-01-31 09:55:21 -0600

    Moving Platform

--- Assets/StarterAssets/Scripts/MovingPlatform.cs
+++ Assets/StarterAssets/Scripts/MovingPlatform.cs
@@ -19,12 +19,47 @@ public class MovingPlatform : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        TargetNextWaypoint();
     }
 
-    // Update is called once per frame
-    void Update()
+   private void FixedUpdate()
+   {
+    _elapsedTime += Time.deltaTime;
+
+    float elapsedPercentage = _elapsedTime / _timeToWaypoint;
+    elapsedPercentage = Mathf.SmoothStep(0,1, elapsedPercentage);
+    transform.position = Vector3.Lerp(_previousWaypoint.position, _targetWaypoint.position, elapsedPercentage);
+
+
+    if (elapsedPercentage >=1)
     {
-        
+        TargetNextWaypoint();
     }
+   }
+   private void TargetNextWaypoint()
+   {
+        _previousWaypoint = _waypointPath.GetWaypoint(_targetWaypointIndex);
+        _targetWaypointIndex = _waypointPath.GetNextWaypoint(_targetWaypointIndex);
+        _targetWaypoint = _waypointPath.GetWaypoint(_targetWaypointIndex);
+
+        _elapsedTime = 0;
+
+        float distanceToWaypoint = Vector3.Distance(_previousWaypoint.position, _targetWaypoint.position);
+
+        _timeToWaypoint = distanceToWaypoint / _speed;
+   }
+   private void OnTriggerEnter(Collider other)
+   {
+    if (other.gameObject.tag == "Player")
+    {
+        other.transform.SetParent(transform);
+    }
+   }
+   private void OnTriggerExit(Collider other)
+   {
+    if (other.gameObject.tag == "Player")
+    {
+        other.transform.SetParent(transform.parent);
+    }
+   }
 }
